15ba4e2d2b9844906e55776b9da6ad36cb250bd9
NIVEDHASHREE <nivedhans23@gmail.com>
2016-07-16 21:17:23 +0530

    Add files via upload

---
+++ stringlength.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class stringlength{
+    public static void main(String[] args) {
+        String s="ram";
+        int n;
+        //Scanner sc= new Scanner(System.in);
+        n=s.length();
+        System.out.println(n);
+    }
+    
+}
