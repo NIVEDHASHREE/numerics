72425e24cab010a38ba39cf6dedb18c45122f975
NIVEDHASHREE <nivedhans23@gmail.com>
2016-07-16 20:56:32 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+
+/**
+ *
+ * @author Sanghavi
+ */
+public class factorial {
+     public static void main(String args[]){
+         int i,n,fact=1;
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter the number");
+         n=sc.nextInt();
+         for(i=1;i<=n;i++)
+         {
+             fact=fact*i;
+             
+         }
+         System.out.println("Factorial of"+n+"is"+fact);
+     }
+}
