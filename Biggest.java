aa9bef296dcd7abf351f81c66c9533a2fb1a23aa
NIVEDHASHREE <nivedhans23@gmail.com>
2016-07-17 10:56:00 +0530

    Add files via upload

---
+++ Biggest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+/**
+ *
+ * @author Sanghavi
+ */
+public class Biggest {
+    public static void main(String args[])
+    {
+        int n1,n2,n3;
+        Scanner sc=new Scanner(System.in);
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        n3=sc.nextInt();
+        if(n1>n2&&n1>n3)
+        {
+            System.out.println("Biggest number is"+n1);
+        }
+        else if(n2>n3)
+        {
+            System.out.println("Biggest number is"+n2);
+        }
+         else
+        {
+            System.out.println("Biggest number is"+n3);
+        }
+    }
+}
