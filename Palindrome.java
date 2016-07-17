c93641f915750dab283dc1c744d2e9ac71e90924
NIVEDHASHREE <nivedhans23@gmail.com>
2016-07-17 11:49:15 +0530

    Add files via upload

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+
+/**
+ *
+ * @author Sanghavi
+ */
+public class Palindrome {
+    public static void main(String args[]){
+        
+        String s1,s2;
+        Scanner sc=new Scanner(System.in);
+        s1=sc.nextLine();
+        System.out.println("the string is:"+s1);
+        s2=new StringBuffer(s1).reverse().toString();
+        System.out.println("the reversed string is:"+s2);
+        if(s1.equalsIgnoreCase(s2))
+        {
+            System.out.println("Palindrome"); 
+        }
+        else
+            System.out.println("not a palindrome");
+        
+    }
+
+}
