66fef0f6985c7e33d4b8c866f4df1d312ce0f52c
NIVEDHASHREE <nivedhans23@gmail.com>
2016-07-17 11:07:55 +0530

    Add files via upload

---
+++ hello5.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class hello5 {
+    public static void main(String[] args) {
+        String s="Hello";
+        int n=0;
+        while(n<5){
+            System.out.println("Hello");
+        }
+    }
+    
+}
