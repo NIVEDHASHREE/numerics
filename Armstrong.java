205a4f9a50c772c5111358fa6c2c378ea85d3379
NIVEDHASHREE <nivedhans23@gmail.com>
2016-07-18 14:40:26 +0530

    Add files via upload

---
+++ Armstrong.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.*;
+/**
+ *
+ * @author Sanghavi
+ */
+public class Armstrong {
+    public static void main(String args[]){
+        int n,n1,n2,sum=0;
+        System.out.println("Enter the number");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        n1=n;
+        while(n1!=0)
+        {
+            n2=n1%10;
+            sum+=n2*n2*n2;
+            n1=n1/10;
+        }
+        if(sum==n)
+            System.out.println("Armstrong numer ");
+        else
+            System.out.println("Not an armstrong ");
+    }
+}
