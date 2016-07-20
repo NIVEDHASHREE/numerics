0a3858c880d7987c4de31e69013cc0e66cd9281c
NIVEDHASHREE <nivedhans23@gmail.com>
2016-07-20 17:40:56 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ * in wipro test pattern
+ */
+public class fibonacci {
+    public static void main(String[] args) {
+        int a=0,b=1,i,n,output1 = 0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(i=0;i<n;i++){
+            output1=a+b;
+            a=b;
+            b=output1;
+            
+        }
+        //System.out.println(output1);
+    }
+    
+}
