66b47f42e576e64dd655951faf08222976e0f052
NIVEDHASHREE <nivedhans23@gmail.com>
2016-07-20 17:15:56 +0530

    Add files via upload

---
+++ allascii.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class allascii {
+    public static void main(String[] args) {
+        String ch;
+        int i;
+        for(i=1; i<=255; i++)
+        {
+                System.out.print((char)i+" - "+i+"\n");
+
+        }
+    }
+    
+}
