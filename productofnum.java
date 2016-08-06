0baabdf02f8935b24aecc5c8a054dd5abdbcbe6a
NIVEDHASHREE <nivedhans23@gmail.com>
2016-08-06 15:17:53 +0530

    Add files via upload

---
+++ productofnum.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package milestone3lc;
+
+/**
+ *
+ * @author user
+ */
+public class productofnum {
+    
+    public static void main(String[] args) {
+        int i,j,k=0;
+        for(j=0;j<50;j++){
+        for(i=0;i<50;i++){
+            if(i+j==25 && i-j==13){
+                System.out.println(i+" "+j);
+               k=i*j; 
+            }
+        }
+    }
+        System.out.println("product is:"+k);
+}
+}
